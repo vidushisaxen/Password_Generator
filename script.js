f0ef3667a4b395c19ffa95d25eef846e0549ef3a
vidushisaxen <vidushisaxenabdn786@gmail.com>
2023-04-26 19:46:32 +0530

    main

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
- 
+ let inputSlider = document.getElementById("inputSlider");
+ let sliderValue = document.getElementById("sliderValue");
+ let passBox = document.getElementById("passBox");
+ let lowercase = document.getElementById("lowercase");
+ let uppercase = document.getElementById("uppercase");
+ let specialchar = document.getElementById("specialchar");
+ let numbers = document.getElementById("numbers");
+ let genBtn = document.getElementById("genBtn");
+
+
+ sliderValue.textContent = inputSlider.value;
+ inputSlider.addEventListener('input',()=>{
+    sliderValue.textContent = inputSlider.value;
+ });
+
+ genBtn.addEventListener('click',()=>{
+passBox.value=generatePassword();
+ });
+
+ let lowerChars = "abcdefghijklmnopqrstuvwxyz";
+ let upperChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+ let number = "0123456789";
+ let specialChars = "~!@#$%^&*";
+
+
+ function generatePassword(){
+    let genpassword ="";
+    let allChars="";
+
+    allChars += lowercase.checked ? lowerChars :"";
+    allChars += uppercase.checked ? upperChars :"";
+    allChars += numbers.checked ? number :"";
+    allChars += specialchar.checked ? specialChars :"";
+
+    let i=1;
+    while(i<=inputSlider.value){
+        genpassword = allChars.charAt(Math.floor(Math.random()*allChars.length));
+        i++;
+    }
+    return genpassword;
+ }
